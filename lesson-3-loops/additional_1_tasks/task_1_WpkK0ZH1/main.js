22e1611aaa09a3f437d9241b17e0710a25cde930
zoryana668 <zoryana668@gmail.com>
2024-11-12 15:22:25 +0200

    Adding HW #3

---
+++ lesson-3-loops/additional_1_tasks/task_1_WpkK0ZH1/main.js
@@ -0,0 +1,16 @@
+// #WpkK0ZH1
+// --створити масив з:
+// - з 5 числових значень
+// - з 5 стрічкових значень
+// - з 5 значень стрічкового, числового та булевого типу
+// - та вивести його в консоль
+
+
+let array = [1, 22, 3, 14, 52];
+console.log(array);
+
+let array1 = ['hi', 'js', 'html', 'react', 'nodejs'];
+console.log(array1);
+
+let array2 = ['js', 'html', 3, true, false];
+console.log(array2);
---
+++ lesson-3-loops/task_7_4WrHwFTEop0/main.js
@@ -0,0 +1,61 @@
+// #4WrHwFTEop0
+// є масив
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+// за допомоги циклу вивести:
+//     - користувачів зі статусом true
+// - користувачів зі статусом false
+// - користувачів які старші за 30 років
+
+
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+//     - користувачі зі статусом true
+
+// for(let user of users){
+//     if(user.status) {
+//         console.log(user);
+//     }
+// }
+
+
+// - користувачі зі статусом false
+
+// for(let user of users) {
+//     if(!user.status) {
+//         console.log(user);
+//     }
+// }
+
+
+
+// - користувачів які старші за 30 років
+
+for(let user of users) {
+    if(user.age > 30) {
+        console.log(user);
+    }
+}
